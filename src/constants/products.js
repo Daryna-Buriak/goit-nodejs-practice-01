a9c61e1fa2996924dd61ab659944e2b02aab69bf
Daryna <buriak.daryna@gmail.com>
2025-05-04 10:35:40 +0300

    Add products.js

---
+++ src/constants/products.js
@@ -0,0 +1,6 @@
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const parthURL = fileURLToPath(import.meta.url);
+const dirname = path.dirname(parthURL);
+const PATH_DB = path.join(dirname, '..', 'db', 'db.json');
